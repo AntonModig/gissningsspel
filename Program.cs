1d2ff19bdea8a6ff7e29c87f391b8110477e283e
AntonModig <anmod3@gmail.com>
2021-09-13 10:29:05 +0200

    lagt upp projektet

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int z = rand.Next(1, 101);
+            int y=0;
+            while(true)
+            {
+                Console.Write("Gissa: ");
+                string str = Console.ReadLine();
+                int x = int.Parse(str);
+                if(x > z)
+                {
+                    Console.WriteLine("Gissning för hög");
+                    y++;
+                }
+                else if (x < z)
+                {
+                    Console.WriteLine("Gissning för låg");
+                    y++;
+                }
+                else if (x == z)
+                {
+                    Console.WriteLine("Rätt Gissning!");
+                    Console.WriteLine("Antal gissningar: " + y++);
+                    break;
+                }
+            }
+        }
+    }
+}
